6058f8bfa634ba9a697a97c3098753bfb14a00c0
henrique_menel <henriqueworks68@gmail.com>
2025-11-28 16:06:55 -0300

    build: criando sistema para exercitar

---
+++ src/main/java/com/musica/api/SistemaMusica/SistemaMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.musica.api.SistemaMusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMusicaApplication.class, args);
+	}
+
+}
